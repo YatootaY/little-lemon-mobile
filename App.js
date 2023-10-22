f50e9b1dcc9e29c490e1de3546129cb2bf070a26
Yato <yatoakii@gmail.com>
2023-10-22 16:55:34 +0630

    feat(OnBoarding): added Input area

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native';
 import OnBoarding from './screens/OnBoarding';
+import {useFonts} from "expo-font"
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    "Karla" : require('./assets/fonts/Karla-Regular.ttf'),
+    "MarkaziText" : require('./assets/fonts/MarkaziText-Regular.ttf'),
+  })
+
+  if (!fontsLoaded){
+    return null;
+  }
+
   return (
     <OnBoarding/>
   );
--- screens/OnBoarding.js
+++ screens/OnBoarding.js
@@ -1,4 +1,4 @@
-import { View, StyleSheet, Text } from "react-native"
+import { View, StyleSheet, Text, TextInput, Pressable } from "react-native"
 import Header from "../components/Header"
 
 const OnBoarding = () => {
@@ -6,6 +6,32 @@ const OnBoarding = () => {
     return(
         <View style={OnBoardingStyle.container}>
             <Header/>
+            <View style={OnBoardingStyle.InputArea}>
+                <View>
+                    <Text style={{color: "#EDEFEE", fontSize: 14, fontFamily: "Karla"}}>Welcome from</Text>
+                    <Text style={{color: "#F4CE14", fontSize: 54, fontFamily: "MarkaziText"}}>Little Lemon</Text>
+                    <Text style={{lineHeight: 36,color: "white",  fontSize: 36, fontFamily: "MarkaziText"}}>Chicago</Text>
+                </View>
+                <View style={{marginTop:16, display: "flex", gap: 20}}>
+                    <View>
+                        <Text style={OnBoardingStyle.InputLabel}>First Name</Text>
+                        <TextInput
+                            style={OnBoardingStyle.InputField}
+                        />
+                    </View>
+                    <View>
+                        <Text style={OnBoardingStyle.InputLabel}>Email</Text>
+                        <TextInput
+                            style={OnBoardingStyle.InputField}
+                        />
+                    </View>
+                </View>
+                <View style={OnBoardingStyle.ButtonContainer}>
+                    <Pressable style={OnBoardingStyle.Button}>
+                        <Text style={{textAlign:"center"}}>Next</Text>
+                    </Pressable>
+                </View>
+            </View>
         </View>
     )
 }
@@ -13,7 +39,41 @@ const OnBoarding = () => {
 const OnBoardingStyle = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    InputArea: {
+        backgroundColor: "#495E57",
+        color: "#EDEFEE",
+        padding: 20,
+        flex: 1,
+        paddingVertical: 50
+    },
+    InputLabel: {
+        color: "#EDEFEE",
+        fontSize: 14,
+        marginBottom: 5,
+        fontWeight: "bold",
+    },
+    InputField: {
+        width: "100%",
+        backgroundColor:"#EDEFEE",
+        color: "#333333",
+        borderRadius: 5,
+        height: 40,
+        padding: 10
+    },
+    ButtonContainer: {
+        marginVertical: 20,
+        alignItems: "flex-end"
+    },
+    Button: {
+        backgroundColor: "#F4CE14",
+        width: 80,
+        paddingVertical: 10,
+        color: "#333333",
+        fontWeight: "bold",
+        borderRadius: 5
     }
+
 })
 
 export default OnBoarding

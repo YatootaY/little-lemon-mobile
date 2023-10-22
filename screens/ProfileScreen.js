c63f85db889bcc89d814dc25aa94530cde08517e
Yato <yatoakii@gmail.com>
2023-10-23 00:05:00 +0630

    feat(Profile): developed UI for Profile

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,9 +1,10 @@
-import { View, Text, Image, StyleSheet, Pressable, TextInput } from "react-native"
+import { ScrollView, View, Text, Image, StyleSheet, Pressable, TextInput } from "react-native"
+import BouncyCheckbox from "react-native-bouncy-checkbox";
 
 const ProfileScreen = () => {
 
     return(
-        <View style={styles.container}>
+        <ScrollView style={styles.container}>
             <Text style={{color:"black", fontSize: 20, fontFamily: "Karla", fontWeight: "bold"}}>Personal information</Text>
             <View>
                 <Text style={styles.label}>Avatar</Text>
@@ -48,12 +49,73 @@ const ProfileScreen = () => {
                     />
                 </View>
                 <Text style={{color:"black", fontSize: 18, fontFamily: "Karla", fontWeight: "bold", marginVertical: 18}}>Email notification</Text>
-                <View>
-                    
+                <View style={{flexDirection: "row", alignItems: "center", marginVertical: 5}}>
+                    <BouncyCheckbox fillColor="#495E57" size={22}/>
+                    <Text style={{color: "#333333", fontFamily: "Karla"}}>Order statuses</Text>
                 </View>
-            
+
+                <View style={{flexDirection: "row", alignItems: "center", marginVertical: 5}}>
+                    <BouncyCheckbox fillColor="#495E57" size={22}/>
+                    <Text style={{color: "#333333", fontFamily: "Karla"}}>Password changes</Text>
+                </View>
+
+                <View style={{flexDirection: "row", alignItems: "center", marginVertical: 5}}>
+                    <BouncyCheckbox fillColor="#495E57" size={22}/>
+                    <Text style={{color: "#333333", fontFamily: "Karla"}}>Special offers</Text>
+                </View>
+
+                <View style={{flexDirection: "row", alignItems: "center", marginVertical: 5}}>
+                    <BouncyCheckbox fillColor="#495E57" size={22}/>
+                    <Text style={{color: "#333333", fontFamily: "Karla"}}>Newsletter</Text>
+                </View>
+                <Pressable>
+                    <Text style={{
+                        backgroundColor: "#F4CE14",
+                        marginVertical: 50,
+                        marginBottom: 10,
+                        textAlign: "center",
+                        paddingVertical: 15,
+                        borderRadius: 10,
+                        fontWeight: "bold"
+                    }}>Log out</Text>
+                </Pressable>
+                <View style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    gap: 30,
+                    marginTop: 20,
+                    marginBottom: 80
+                }}>
+                    <Pressable>
+                        <Text style={{
+                            width: 130,
+                            paddingVertical: 10,
+                            textAlign: "center",
+                            color: "white",
+                            fontWeight: "bold",
+                            fontFamily: "Karla",
+                            borderColor: "gray",
+                            borderWidth: 1,
+                            color: "gray"
+                        }}>Discard changes</Text>
+                    </Pressable>
+                    <Pressable>
+                        <Text style={{
+                            width: 130,
+                            paddingVertical: 10,
+                            textAlign: "center",
+                            color: "white",
+                            fontWeight: "bold",
+                            fontFamily: "Karla",
+                            backgroundColor: "#495E57"
+                        }}>
+                            Save changes
+                        </Text>
+                    </Pressable>
+                </View>
             </View>
-        </View>
+        </ScrollView>
     )
 }
 

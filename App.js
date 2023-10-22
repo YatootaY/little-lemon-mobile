8a2019970371b9bfaf477f80cedf9b5551ea86c9
Yato <yatoakii@gmail.com>
2023-10-22 19:48:18 +0630

    feat(navigation): setup navigation

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native';
 import OnBoarding from './screens/OnBoarding';
 import {useFonts} from "expo-font"
+import {NavigationContainer} from "@react-navigation/native"
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
 
@@ -14,7 +18,11 @@ export default function App() {
   }
 
   return (
-    <OnBoarding/>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Onboarding" component={OnBoarding}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
